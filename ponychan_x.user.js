7e8a838a4d7fd99de468902a5140c81ca9f55a5f
milkytiptoe <directxpert@gmail.com>
2012-08-28 02:32:59 +1000

    rename and restructure some basic settings

--- ponychan_x.user.js
+++ ponychan_x.user.js
@@ -8,3 +8,82 @@
 // @exclude       http://www.ponychan.net/chan/board.php
 // @icon          http://www.milkyis.me/ponychanx/icon.png
 // ==/UserScript==
+
+// Inspired by 4chan X
+// http://mayhemydg.github.com/4chan-x/
+
+var AutoUpdate, Favicon, Filter, Keybinds, Main, Ponychan, QR, Settings, Updater;
+
+AutoUpdate = {
+	init: function() {
+	
+	}
+};
+
+Favicon = {
+
+};
+
+Filter = {
+
+};
+
+Keybinds = {
+
+};
+
+Main = {
+	init: function() {
+		
+	}
+};
+
+Ponychan = {
+	
+};
+
+QR = {
+	init: function() {
+		
+	}
+};
+
+Settings = {
+	settings: {
+		Updater: {
+			"Enable thread autoupdate": true,
+			"Enabled watched threads autoupdate": true
+		},
+		Images: {
+			"Show image on hover": true,
+			"Show spoilered images": true,
+			"Animate gif thumbnails": true
+		},
+		Monitoring: {
+			"Enable read and unread favicons": true,
+			"Show unread post count in title": true
+		},
+		Posting: {
+			"Enable quick reply": true
+		},
+		Posts: {
+			"Enable backlinks": true,
+			"Enable inline posts": true,
+			"Show reply link": true,
+			"Show report link": true,
+			"Show google image link": true,
+			"Show save image link": true
+		},
+		Other: {
+			"Automatically check for updates": true
+		}
+	}
+};
+
+Updater = {
+	init: function() {
+	
+	}
+};
+
+Main.init();
