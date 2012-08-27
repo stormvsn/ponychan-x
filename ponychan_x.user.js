306843258cd8fb78ddae4ce2fe4481ad2628c24e
milkytiptoe <directxpert@gmail.com>
2012-08-28 01:23:22 +1000

    nuke old code for rewrite

---
+++ ponychan_x.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name          Ponychan X
+// @version       1.0
+// @description   Adds new features to ponychan
+// @namespace     milky
+// @author        milky
+// @include       http://www.ponychan.net/chan/*
+// @exclude       http://www.ponychan.net/chan/board.php
+// @icon          http://www.milkyis.me/ponychanx/icon.png
+// ==/UserScript==
